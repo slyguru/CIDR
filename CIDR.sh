09551b7eacaeb73d0b13223878608fbb697d281d
Brian Wafula <brian@techika.net>
2024-03-01 10:31:57 +0300

    All changes made

--- CIDR.sh
+++ CIDR.sh
@@ -13,7 +13,9 @@ fi
 function network_range {
 	for ip in $ipaddr
 	do
-		netrange=$(whois $ip | grep "NetRange\|CIDR" | tee -a CIDR.txt)cidr=$(whois $ip | grep "CIDR" | awk '{print $2}')cidr_ips=$(prips $cidr)
+		netrange=$(whois $ip | grep "NetRange\|CIDR" | tee -a CIDR.txt)
+		cidr=$(whois $ip | grep "CIDR" | awk '{print $2}')
+		cidr_ips=$(prips $cidr)
 		echo -e "\nNetRange for $ip:"
 		echo -e "$netrange"
 	done
